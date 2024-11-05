063e2342c8fba9b59fd763523f3d95f32e0195d9
madjetmax <kirilchyhsnchik@gmail.com>
2024-11-05 14:42:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ async def main_page(response: Response):
 
 if __name__ == "__main__":
     try:
-        uvicorn.run(app, host="127.0.0.1", port=os.environ.get("PORT"))
+        uvicorn.run(app, host="127.0.0.1", port=int(os.environ.get("PORT")))
     except KeyboardInterrupt:
         print("stoped!")
